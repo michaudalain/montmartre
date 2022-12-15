dec48910ec91d6a229fd033e6c8ee2eaef5e37bc
michaudalain <107669315+michaudalain@users.noreply.github.com>
2022-12-15 16:31:13 +0100

    Footer et Header independants

---
+++ presentation.php
@@ -0,0 +1,79 @@
+<?php
+require_once "header.php";
+?>
+<div class="container">
+    <div class="brand-xl">
+        <p >Au Coeur de Montmartre</p>
+    </div>
+</div>
+<main>
+    <div class="content-half-header">      
+        <div class="container">
+            <h1>Terrasse avec vue imprenable</h1>
+            <div class="flex-container-col">
+                <div class="flex-galery-container">
+                    <div class="flex-container-wd60">
+                        <a href="#" data-fancybox="gallerie" data-src="img/terrasse.webp" data-caption="La terrasse">
+                            <img class="img-max-100 img-scale-anim" src="img/terrasse.webp" alt="Vue sur les toits de Paris">
+                        </a>
+                    </div>
+                    <div class="flex-container-col-wd20">
+                        <a href="#" data-fancybox="gallerie" data-src="img/Interieur-1.webp" data-caption="Le coin repas">
+                            <img class="img-max-100 img-scale-anim" src="img/Interieur-1.webp" alt="Coin repas">
+                        </a>
+                        <a href="#" data-fancybox="gallerie" data-src="img/Interieur-2.webp" data-caption="La chambre">
+                            <img class="img-max-100 img-scale-anim" src="img/Interieur-2.webp" alt="Chambre">
+                        </a>
+                        <a href="#" data-fancybox="gallerie" data-src="img/sejour2.webp" data-caption="Le séjour">
+                            <img class="img-max-100 img-scale-anim" src="img/sejour2.webp" alt="Le séjour">
+                        </a>
+                    </div>
+                    <div class="flex-container-col-wd20">
+                        <a href="#" data-fancybox="gallerie" data-src="img/sejour.webp" data-caption="Le séjour">
+                            <img class="img-max-100 img-scale-anim" src="img/sejour.webp" alt="Le séjour">
+                        </a>
+                        <a href="#" data-fancybox="gallerie" data-src="img/Vue toits.webp" data-caption="Vue sur les toits de Paris">
+                            <img class="img-max-100 img-scale-anim" src="img/Vue toits.webp" alt="Vue sur les toits de Paris">
+                        </a>
+                        <a href="#" data-fancybox="gallerie" data-src="img/cuisine.webp" data-caption="Vue sur les toits de Paris">
+                            <img class="img-max-100 img-scale-anim" src="img/cuisine.webp" alt="La cuisine">
+                        </a>
+                    </div>
+                </div>
+                <div class="block-separator">
+                    <h2>Le logement</h2>
+                    <p class="main-description">
+                        Petit appartement baigné de lumière,confortable et chaleureux,au 7ème et dernier étage avec un ascenseur,exposé plein sud,donnant sur une jolie terrasse qui domine tout Paris où vous pourrez prendre vos repas et vous détendre en admirant la ville et ses plus beaux monuments à toute heure du jour et de la nuit.
+                    </p>
+                    <p>
+                        Situé juste au pied du <strong>Sacré Coeur</strong> et du funiculasre qui permet de monter au sommet de la butte Montmartre,dans une petite rue au calme à l'écart de l'agitation touristique,cet appartement de charme est idéalement situé pour un séjour ou vous pourrez à la fois :- profiter, à 5 minutes à pied, de nombreux commerces et d'une vie de quartier riche et animée- et rejoindre rapidement et très facilement, grâce aux trois stations de <strong>métro Anvers (ligne 2), Abbesses (ligne 12) et Barbes-Rochechouart (ligne 4)</strong>, le centre de Paris, les gares et la plupart des lieux que vous aurez à visiter durant votre séjour.
+                    </p>
+                    <p>
+                        L'appartement vient d'être refait à neuf et nouvellement inscrit sur <strong>AIRBNB</strong> .La cuisine, ouverte sur le salon, est entièrement équipée pour pouvoir cuisiner agréablement (frigidaire, grill, four, induction, casseroles, bouilloire, ustensiles,...)La chambre, où vous pourrez dormir dans un lit deux places (140X200) et vous réveiller avec une vue merveilleuse sur le ciel de Paris, donne accès à un second petit balcon privatif sur lequel il sera bon de boire votre thé ou votre café du matin.Le salon est équipé d'un canapé convertible en un lit d'appoint (140X190) pour accueillir si besoin une ou deux personnes supplémentaires.La petite salle de bain, fonctionnelle et claire, est équipée d'une douche et d'un WC.TV, lecteur DVD Blue Ray, connexion internet WIFI, aspirateur, penderie, fer à repasser,... sont à votre disposition pendant votre séjour et vous trouverez si besoin une laverie un peu plus loin dans la même rue.
+                    </p>
+                    <p>
+                        Nous nous rendrons disponible si vous le souhaitez pour des conseils ou des bonnes adresses et faciliter votre séjour.Nous avons hâte de recevoir nos premiers voyageurs et recueillir leurs impressions !
+                    </p>
+                </div>
+                <div class="block-separator">
+                    <h2>Ce que propose le logement</h2>
+                    <div class="flex-list">
+                        <i class="fa-solid fa-kitchen-set"></i><p>Cuisine</p>
+                    </div>
+                    <div class="flex-list">
+                        <i class="fa-solid fa-tv"></i><p>TV avec abonnement standard au câble</p>
+                    </div>
+                    <div class="flex-list">
+                        <i class="fa-solid fa-wifi"></i><p>Wifi</p>
+                    </div>
+                    <div class="flex-list">
+                        <i class="fa-solid fa-elevator"></i><p>Ascenceur</p>
+                    </div>
+                </div>
+            </div>
+        </div>            
+    </div>
+</main>
+<?php
+require_once "footer.php";
+?>
